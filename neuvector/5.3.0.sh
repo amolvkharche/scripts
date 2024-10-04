06375abdacc7d6dddeaa8357789a183ecdb92770
amolvkharche <83961171+amolvkharche@users.noreply.github.com>
2024-10-04 22:33:33 +0530

    Update 5.3.0.sh

--- neuvector/5.3.0.sh
+++ neuvector/5.3.0.sh
@@ -1,7 +1,7 @@
 echo -e "Enter the neuvector version you want to install : "
 read version
 
-if [[ "$version" != "5.3.2" && "$version" != "5.3.3" && "$version" != "5.3.4" ]]; then
+if [ "$version" != "5.3.2" && "$version" != "5.3.3" && "$version" != "5.3.4" ]; then
     echo "Version $version is not supported. Exiting."
     exit 1
 fi
